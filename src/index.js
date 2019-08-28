2c597881b33a37f98386e9acf5d119fb06d343be
Piotr Kowalski <piecioshka@gmail.com>
2019-08-28 13:56:10 +0200

    Add node-fetch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-console.log("root", process.version);
-const ora = require("ora");
-const spinner = ora("Loading...").start();
+const fetch = require('node-fetch');
 
-setTimeout(() => {
-    spinner.succeed("Done!\n");
-}, 1000);
+const url = `http://192.168.0.227/@json/50.json`
+
+async function go() {
+    console.log('fn go called');
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+}
+
+go().catch(console.log);
